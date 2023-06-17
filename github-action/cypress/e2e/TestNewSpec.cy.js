4538fc0cb190a0f11bacfd819bd7d9166bde6cf1
Nivisan Jeyananthan <33133411+Nivisan-Jeyananthan@users.noreply.github.com>
2023-06-17 17:39:27 +0000

    feat!(Actions): replace old project

--- github-action/cypress/e2e/TestNewSpec.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Navigation", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:3000/");
-    cy.get("h1").contains("Vite");
-  });
-});
-
-// prevent reading typescript this file as legacy
-export {};
